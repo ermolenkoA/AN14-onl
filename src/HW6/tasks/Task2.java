020dcb60c2fef6b0f91ec9ea18fbe4215a065e8f
Artem Ermolenko <kypalo2012@gmail.com>
2023-05-24 03:21:35 +0300

    Added Task2 to HW6

--- src/HW6/tasks/Task2.java
+++ src/HW6/tasks/Task2.java
@@ -1,6 +1,8 @@
 package HW6.tasks;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task2 {
     /*
@@ -13,7 +15,40 @@ public class Task2 {
      * Про нарушении формата входной строки бросить исключение IllegalArgumentException
      */
     public static void start(Scanner in) {
-        System.out.print("Задание 2:\n");
+        System.out.println("Задание 2:\nВведите выражение:");
+        String expression = in.nextLine();
+        try {
+            System.out.printf("Результат выражения - %d", getExpressionResult(expression));
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
+    public static int getExpressionResult(String input) throws IllegalArgumentException{
+
+        if(!input.matches("([+-] )?\\d+( [+-] \\d+)*")){
+            throw new IllegalArgumentException("Ошибка: введенные не подходят по формату.");
+        }
+
+        Pattern pattern = Pattern.compile("([+-] )?(\\d+)");
+        Matcher matcher = pattern.matcher(input);
+
+        matcher.find();
+        String sign = matcher.group(1);
+        int result = Integer.parseInt(matcher.group(2));
+
+        if (sign != null && sign.contains("-")) {
+            result *= -1;
+        }
+        matcher.usePattern(Pattern.compile(" ([+-]) (\\d+)"));
+
+        while (matcher.find()) {
+            sign = matcher.group(1);
+            int next = Integer.parseInt(matcher.group(2));
+
+            result = result + (sign.equals("-") ? -next : next);
+        }
+
+        return result;
+    }
 }
